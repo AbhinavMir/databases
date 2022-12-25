45bfef1e71a30071f48e65c14b7c36dbe3739640
Abhinav Srivastava <atg271@gmail.com>
2022-12-25 00:45:45 -0500

    wip

--- src/log.py
+++ src/log.py
@@ -1,30 +1,27 @@
 import enumDef as enum
+import os
 
 logPath = "../utils/log.csv"
+#logSeq,prevLSN,recordType,transactionID,pageID,length,offset,oldData,newData
+logColumns = ["logSeq", "prevLSN", "recordType", "transactionID", "pageID", "length", "offset", "oldData", "newData"]
+logData = {}
 
 class logFile:
     def __init__(self):
-        try:
+        # check if log file exists
+        if(os.path.exists(logPath)):
+            # check if log file has been initialized
             with open(logPath, 'r') as f:
-                pass
-        except FileNotFoundError:
-            self.logSeq = 0
-            self.prevLSN = None
-            self.recordType = None
-            self.transactionID = None
-            self.pageID = None
-            self.length = None
-            self.offset = None
-            self.oldData = None
-            self.newData = None
-            with open(logPath, 'w') as f:
-                f.write("logSeq,prevLSN,recordType,transactionID,pageID,length,offset,oldData,newData")
-
-    def __str__(self):
-        return f"{self.logSeq},{self.prevLSN},{self.recordType},{self.transactionID},{self.pageID},{self.length},{self.offset},{self.oldData},{self.newData}"
+                if(f.readline() == "logSeq,prevLSN,recordType,transactionID,pageID,length,offset,oldData,newData\n"):
+                    logData = helperFunctions.loadDataToDictionary()
+                else:
+                    with open(logPath, 'w') as f:
+                        f.write("logSeq,prevLSN,recordType,transactionID,pageID,length,offset,oldData,newData\n")
+                        f.write(f"0, NA, NA, NA, NA, NA, NA, NA, NA " + "\n")
 
 class Log:
-    def __init__(self, prevLSN, recordType, transactionID, pageID, length, offset, oldData, newData):
+    def __init__(self, LSN, prevLSN, recordType, transactionID, pageID, length, offset, oldData, newData):
+        self.LSN = LSN
         self.prevLSN = prevLSN
         self.recordType = recordType
         self.transactionID = transactionID
@@ -33,8 +30,35 @@ class Log:
         self.offset = offset
         self.oldData = oldData
         self.newData = newData
-        with open(logPath, 'r') as f:
-            self.logSeq = len(f.readlines())
+        with open(logPath, 'a') as f:
+            f.write(f"{self.LSN},{self.prevLSN},{self.recordType},{self.transactionID},{self.pageID},{self.length},{self.offset},{self.oldData},{self.newData}" + "\n")
 
-genLog = Log(1, 0, enum.xactAction.UPDATE, 1, 1, 1, 1, "oldData", "newData")
-genLog.writeLog("../utils/log.csv")
+    def __str__(self):
+        return f"{self.LSN},{self.prevLSN},{self.recordType},{self.transactionID},{self.pageID},{self.length},{self.offset},{self.oldData},{self.newData}"+"\n"
+
+class helperFunctions:
+    def parseDataFromLog():
+        with open(logPath, 'r') as f:
+            data = f.readlines()
+            data = [x.strip() for x in data]
+            data = data[1:]
+            return data
+    
+    def loadDataToDictionary():
+        data = helperFunctions.parseDataFromLog()
+        for i in range(len(data)):
+            logData[i] = {}
+            for j in range(len(logColumns)):
+                logData[i][logColumns[j]] = data[i].split(',')[j]
+        return logData
+    
+    def getLatestLSN():
+        data = helperFunctions.parseDataFromLog()
+        return len(data)-1
+
+class Main:
+    def initializeLogProcess():
+        return logFile()
+    
+    def appendLog(LSN, prevLSN, recordType, transactionID, pageID, length, offset, oldData, newData):
+        return Log(LSN, prevLSN, recordType, transactionID, pageID, length, offset, oldData, newData)
--- src/record.py
+++ src/record.py
@@ -2,6 +2,7 @@
 This module contains the Record class. Records are Tuple objects that store a column of data in the database.
 '''
 class Record:
-    def __init__(self, recordID, pageID):
+    def __init__(self, recordID, pageID, values[]):
         self.recordID = recordID
         self.pageID = pageID
+        self.values = values
